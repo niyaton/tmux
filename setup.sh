05779cabf0386c45587d299427706f83d65cb32f
Kenji Fujiwara <niyaton@gmail.com>
2017-11-13 17:11:58 +0900

    automatic install tpm (not install tpm packages)

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 ln -i -n -s $PWD/tmux.conf ~/.tmux.conf
 [ -d ~/.tmux.d ] || mkdir ~/.tmux.d
 ln -i -n -s $PWD/tmux.conf.osx ~/.tmux.d/tmux.conf.osx
+echo "cloning tpm package manager git repository"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
